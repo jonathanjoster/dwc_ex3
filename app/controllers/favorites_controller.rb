adacb7c9e9cfc7e6194a3e5bbdbff58a833c9c44
jonathanjoster <53422031+jonathanjoster@users.noreply.github.com>
2023-05-22 14:09:46 +0000

    [Finish]ex4

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,4 @@
 class FavoritesController < ApplicationController
-  # before_action :is_matching_login_user
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
@@ -12,12 +11,5 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
     redirect_to request.referer
-  end
-
-  def is_matching_login_user
-    favorite = current_user.favorites.find_by(book_id: params[:book_id])
-    unless current_user.favorites.exists?(book_id: params[:book_id])
-      redirect_to request.referer
-    end
   end
 end
